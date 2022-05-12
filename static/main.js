efc367359a958800eb08cb11f4a8000c3ea7b1a2
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-05-12 19:14:22 +0200

    Switched to json and added join/leave messages

--- static/main.js
+++ static/main.js
@@ -2,9 +2,44 @@ function staticUrl(url) {
     return `${window.pnpZone.static}plugins/pnp-chat/${url}`;
 }
 
+const JOIN = "join";
+const MESSAGE = "message";
+const LEAVE = "leave";
+
+class ChatConnection {
+    constructor(username) {
+        const {host, protocol} = window.location;
+        this.socket = new WebSocket(`${protocol.replace("http", "ws")}//${host}/chat/socket`);
+        this.username = username;
+    }
+
+    join() {
+        this.socket.send(JSON.stringify({
+            type: JOIN,
+            user: this.username,
+            msg: null,
+        }));
+    }
+
+    send(msg) {
+        this.socket.send(JSON.stringify({
+            type: MESSAGE,
+            user: this.username,
+            msg,
+        }));
+    }
+
+    leave() {
+        this.socket.send(JSON.stringify({
+            type: LEAVE,
+            user: this.username,
+            msg: null,
+        }));
+    }
+}
+
 export default async function init() {
-    const {host, protocol} = window.location;
-    const socket = new WebSocket(`${protocol.replace("http", "ws")}//${host}/chat/socket`);
+    const chat = new ChatConnection("Anonymous");
 
     const screen = await window.pnpZone.waw;
     const win = await screen.newWindow({dock: "left", title: "Chat", icon: staticUrl("icon.svg")});
@@ -20,15 +55,28 @@ export default async function init() {
     const form = win.querySelector("form");
     form.onsubmit = function (event) {
         event.preventDefault();
-        socket.send(form[0].value);
+        chat.send(form[0].value);
         form[0].value = "";
     };
 
     const history = win.querySelector(".history");
-    socket.onmessage = function (message) {
+    chat.socket.onmessage = function (message) {
+        const {type, user, msg} = JSON.parse(message.data);
         const container = document.createElement("div");
-        container.innerText = message.data;
+        switch (type) {
+            case JOIN:
+                container.innerText = `${user} joined the chat`; break;
+            case MESSAGE:
+                container.innerText = `<${user}> ${msg}`; break;
+            case LEAVE:
+                container.innerText = `${user} left the chat`; break;
+        }
         history.append(container);
     };
+
+    chat.join();
+    window.addEventListener("beforeunload", function (event) {
+        chat.leave();
+    });
 }
 
